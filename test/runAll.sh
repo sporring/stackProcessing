f7b1978dffe70a66f3f234e28b321505ae633bc4
Jon Sporring <sporring@di.ku.dk>
2025-10-14 11:53:25 +0200

    added tracking of max memory and number of images, cleanup

--- test/runAll.sh
+++ test/runAll.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-for i in *.fsproj; do \
-  echo $i && \
-  dotnet clean --verbosity q $i && \
-  dotnet build --verbosity q $i && \
-  /usr/bin/time -l env DYLD_LIBRARY_PATH="$PWD/lib" \
-    dotnet run debug 30 --verbosity q --project $i > $i.out 2>&1; \
-  done;
+dirs=(**/*.fsproj(:h))
+for i in $dirs; do
+  echo $i
+  pushd $i
+  /usr/bin/time env DYLD_LIBRARY_PATH="$(pwd)/lib" dotnet run debug 18 --verbosity q > ../$i.out 2>&1; \
+  popd
+done;
