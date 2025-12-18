6f246fa72575dcc130408a0a667f696279790d19
Jon Sporring <sporring@di.ku.dk>
2025-12-18 14:55:54 +0100

    reorganised file structure and implemented a solution file

---
+++ samples/multiplyMask/multiplyMask.fs
@@ -0,0 +1,34 @@
+﻿// To run, remember to:
+// export DYLD_LIBRARY_PATH=./StackPipeline/lib:$(pwd)/bin/Debug/net8.0
+open StackProcessing
+
+[<EntryPoint>]
+let main arg =
+    let availableMemory = 2UL * 1024UL * 1024UL * 1024UL // 2GB for example
+
+    let src = 
+        if arg.Length > 0 && arg[0] = "debug" then
+            debug availableMemory
+        else
+            source availableMemory
+    let input,mask,output = 
+        if arg.Length > 1 then
+            $"image{arg[1]}", $"mask{arg[1]}", $"result{arg[1]}"
+        else
+            "image18", "mask18", "result18"
+
+    let imageMaker =
+        src
+        |> read<uint8> ("../"+input) ".tiff"
+    let maskMaker =
+        src
+        |> read<uint8> ("../"+mask) ".tiff"
+
+    (imageMaker, maskMaker) ||> zip
+    >=> tap "[tab] For mul2"
+    >>=> mulPair
+    >=> tap "[tab] For write"
+    >=> write ("../"+output) ".tiff"
+    |> sink
+
+    0
---
+++ samples/sharedStreamer/sharedStreamer.fs
@@ -0,0 +1,39 @@
+﻿// To run, remember to:
+// export DYLD_LIBRARY_PATH=./StackPipeline/lib:$(pwd)/bin/Debug/net8.0
+open StackProcessing
+
+[<EntryPoint>]
+let main arg =
+    let availableMemory = 2UL * 1024UL * 1024UL *1024UL // 2GB for example
+
+    let src = 
+        if arg.Length > 0 && arg[0] = "debug" then
+            debug availableMemory
+        else
+            source availableMemory
+    let input,output = 
+        if arg.Length > 1 then
+            $"image{arg[1]}", $"result{arg[1]}"
+        else
+            "image18", "result18"
+
+
+    let readMaker = 
+        src
+        |> read<uint8> ("../"+input) ".tiff"
+        //>=> tapIt (fun s -> $"[readAs] {s.Index} -> Image {s.Image}")
+
+    readMaker 
+    >=> tap "cast"
+    >=> cast<uint8,float>
+    >=> tap "fan out"
+    >=>> (imageAddScalar 1.0, imageAddScalar 2.0)
+    >=> tap "fan in"
+    >>=> mulPair
+    >=> tap "cast"
+    >=> cast<float,int8>
+    >=> tap "write"
+    >=> write ("../"+output) ".tiff"
+    |> sink
+
+    0
