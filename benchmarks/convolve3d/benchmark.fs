613bdca80e6f82262fc911802a028a50b3fca021
David Stansby <dstansby@gmail.com>
2026-04-24 15:15:53 +0100

    Add basic convolve3d benchmark

---
+++ benchmarks/convolve3d/benchmark.fs
@@ -0,0 +1,28 @@
+// To run, remember to:
+// export DYLD_LIBRARY_PATH=$(pwd)/lib
+open StackProcessing
+
+[<EntryPoint>]
+let main arg =
+    let availableMemory = 2UL * 1024UL * 1024UL *1024UL // 1MB for example
+    let sigma = 1.0
+
+    let src = source availableMemory
+    let input, output =
+        if arg.Length = 2 then
+            $"{arg[0]}", $"{arg[1]}"
+        else
+            failwith "input, output not provided as command line arguments"
+
+    src
+    |> read<float> input ".tiff"
+    // sigma = 1 => pad=2, depth = 22 => integer solution for number of strides when:
+    // windowSize = 1, 6, 15, or 26, => n = 21, 10, 1, or 0
+    >=> discreteGaussian sigma None None (Some 15u)
+    //>=> convGauss sigma
+    >=> cast<float,uint8>
+    >=> write output ".tiff"
+    //>=> ignoreImages ()
+    |> sink
+
+    0
