5ab6c8a2a564a29754396f578ba347949f77e66e
Jon Sporring <sporring@di.ku.dk>
2026-05-04 20:45:46 +0200

    corrected a bunch of errors in Image.Test and stuff

--- src/Image/simpleTest.fsx
+++ src/Image/simpleTest.fsx
@@ -2,78 +2,104 @@
 #r "bin/Debug/net10.0/SimpleITKCSharpManaged.dll"
 open Image
 open ImageFunctions
-let I = Image<int>([10u;12u])
-let J = Image<int list>([10u;12u])
-printfn "%A" I[1,2]
-I[1,2] <- 1
-printfn "%A" I[1,2]
-printfn "%A" J[2,1]
-J[1,2] <- [2;1]
-printfn "%A" J[1,2];;
-let K = Image<int list>([10u;1u;12u]);;
-printfn "%A" K
-let L = ImageFunctions.squeeze K
-printfn "%A" K
-printfn "%A" L;;
 
-let O = I + 5
-let M = ImageFunctions.concatAlong 0u I O
-printfn "%A" M
-let N = ImageFunctions.concatAlong 1u I O
-printfn "%A" N
-let P = ImageFunctions.concatAlong 2u I O
-printfn "%A" P
-let Q = ImageFunctions.concatAlong 3u I O
-printfn "%A" Q;;
+let makeImpulse2D () =
+    Array2D.init 5 5 (fun m n -> if m = 2 && n = 2 then 1.0 else 0.0)
 
-let arr = array2D [ [ 10.0f; 20.0f ]; [ 30.0f; 40.0f ] ]
-let imgF = Image<float32>.ofArray2D arr
-let imgB = imgF.cast<uint8>();;
+do
+    let scalarImage = Image<int>([10u; 12u])
+    let vectorImage = Image<int list>([10u; 12u])
 
-let img = Image<float>.ofArray2D (array2D [ [ 1.0; 2.0 ]; [ 3.0; 4.0 ] ])
-let ker = Image<float>.ofArray2D (array2D [ [ 0.0; 0.0; 0.0 ]; [ 0.0; 1.0; 0.0 ]; [ 0.0; 0.0; 0.0 ] ])
-let result = ImageFunctions.conv img ker;;
+    printfn "%A" scalarImage[1, 2]
+    scalarImage[1, 2] <- 1
+    printfn "%A" scalarImage[1, 2]
 
-let arr = Array2D.init 5 5 (fun m n -> if m=2 && n=2 then 1.0 else 0.0)
-let img = Image<float>.ofArray2D arr
-let blurred = ImageFunctions.discreteGaussian 1.0 img;;
+    printfn "%A" vectorImage[2, 1]
+    vectorImage[1, 2] <- [2; 1]
+    printfn "%A" vectorImage[1, 2]
 
-let arr = Array2D.init 5 5 (fun m n -> if m=2 && n=2 then 1.0 else 0.0)
-let img = Image<float>.ofArray2D arr
-let blurred = ImageFunctions.recursiveGaussian 1.0 0u img;;
+    let squeezedSource = Image<int list>([10u; 1u; 12u])
+    printfn "%A" squeezedSource
 
-let arr = Array2D.init 5 5 (fun m n -> if m=2 && n=2 then 1.0 else 0.0)
-let img = Image<float>.ofArray2D arr
-let lap = ImageFunctions.laplacianConvolve 1.0 img;;
+    let squeezed = ImageFunctions.squeeze squeezedSource
+    printfn "%A" squeezedSource
+    printfn "%A" squeezed
 
-let arr = Array2D.init 5 5 (fun m n -> if m=2 && n=2 then 1.0 else 0.0)
-let img = Image<float>.ofArray2D arr
-let grad = ImageFunctions.gradientConvolve 0u 1u img;;
+    let shifted = ImageFunctions.imageAddScalar scalarImage 5
+    printfn "%A" (ImageFunctions.concatAlong 0u scalarImage shifted)
+    printfn "%A" (ImageFunctions.concatAlong 1u scalarImage shifted)
+    printfn "%A" (ImageFunctions.concatAlong 2u scalarImage shifted)
+    printfn "%A" (ImageFunctions.concatAlong 3u scalarImage shifted)
 
-let img = Image<int>.ofArray2D (array2D [[0;1;1;0]])
-let eroded = ImageFunctions.binaryErode 1u 1.0 img;;
+do
+    let sourceArray = array2D [[10.0f; 20.0f]; [30.0f; 40.0f]]
+    let floatImage = Image<float32>.ofArray2D sourceArray
+    let byteImage = floatImage.castTo<uint8>()
+    printfn "%A" byteImage
 
-let img = Image<int>.ofArray2D (array2D [[0;1;0]])
-let dilated = ImageFunctions.binaryDilate 1u 1.0 img;;
+do
+    let image = Image<float>.ofArray2D (array2D [[1.0; 2.0]; [3.0; 4.0]])
+    let kernel = Image<float>.ofArray2D (array2D [[0.0; 0.0; 0.0]; [0.0; 1.0; 0.0]; [0.0; 0.0; 0.0]])
+    let result = ImageFunctions.conv image kernel
+    printfn "%A" result
 
-let img = Image<int>.ofArray2D (array2D [[0;1;0;1;0]])
-let opened = ImageFunctions.binaryOpening 1u 1.0 img;;
+do
+    let image = Image<float>.ofArray2D (makeImpulse2D ())
+    let blurred = ImageFunctions.discreteGaussian 2u 1.0 None None None image
+    printfn "%A" blurred
 
-let img = Image<int>.ofArray2D (array2D [[1;0;1]])
-let closed = ImageFunctions.binaryClosing 1u 1.0 img;;
+do
+    let image = Image<float>.ofArray2D (makeImpulse2D ())
+    let blurredValid = ImageFunctions.discreteGaussian 2u 1.0 None (Some ImageFunctions.Valid) None image
+    printfn "%A" blurredValid
 
-let img = Image<int>.ofArray2D (array2D [[1;1;1]; [1;0;1]; [1;1;1]])
-let filled = ImageFunctions.binaryFillHoles 1.0 img;;
+do
+    let image = Image<float>.ofArray2D (makeImpulse2D ())
+    let gradient = ImageFunctions.gradientConvolve 0u 1u image
+    printfn "%A" gradient
 
-let img = Image<int>.ofArray2D (array2D [[1;0;1]])
-let cc = (ImageFunctions.connectedComponents img).Labels;;
+do
+    let image = Image<uint8>.ofArray2D (array2D [[0uy; 1uy; 1uy; 0uy]])
+    let eroded = ImageFunctions.binaryErode 1u image
+    printfn "%A" eroded
 
-let img = Image<int>.ofArray2D (array2D [[1;0;1]])
-let cc = (ImageFunctions.connectedComponents img).Labels
-let relabeled = ImageFunctions.relabelComponents 2u cc;;
+do
+    let image = Image<uint8>.ofArray2D (array2D [[0uy; 1uy; 0uy]])
+    let dilated = ImageFunctions.binaryDilate 1u image
+    printfn "%A" dilated
 
-let img = Image<int>.ofArray2D (array2D [[0;1;0]])
-let dmap = ImageFunctions.signedDistanceMap img;;
+do
+    let image = Image<uint8>.ofArray2D (array2D [[0uy; 1uy; 0uy; 1uy; 0uy]])
+    let opened = ImageFunctions.binaryOpening 1u image
+    printfn "%A" opened
 
-let img = Image<int>.ofArray2D (array2D [[0;1;0;1;0]])
-let ws = ImageFunctions.watershed 0.0 img;;
+do
+    let image = Image<uint8>.ofArray2D (array2D [[1uy; 0uy; 1uy]])
+    let closed = ImageFunctions.binaryClosing 1u image
+    printfn "%A" closed
+
+do
+    let image = Image<uint8>.ofArray2D (array2D [[1uy; 1uy; 1uy]; [1uy; 0uy; 1uy]; [1uy; 1uy; 1uy]])
+    let filled = ImageFunctions.binaryFillHoles image
+    printfn "%A" filled
+
+do
+    let image = Image<uint8>.ofArray2D (array2D [[1uy; 0uy; 1uy]])
+    let labels = (ImageFunctions.connectedComponents image).Labels
+    printfn "%A" labels
+
+do
+    let image = Image<uint8>.ofArray2D (array2D [[1uy; 0uy; 1uy]])
+    let labels = (ImageFunctions.connectedComponents image).Labels
+    let relabeled = ImageFunctions.relabelComponents 2u labels
+    printfn "%A" relabeled
+
+do
+    let image = Image<uint8>.ofArray2D (array2D [[0uy; 1uy; 0uy]])
+    let distanceMap = ImageFunctions.signedDistanceMap 1uy 0uy image
+    printfn "%A" distanceMap
+
+do
+    let image = Image<uint8>.ofArray2D (array2D [[0uy; 1uy; 0uy; 1uy; 0uy]])
+    let watershed = ImageFunctions.watershed 0.0 image
+    printfn "%A" watershed
