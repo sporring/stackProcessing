5719f64071ac1c65440dedc390c814b5a4f11743
Jon Sporring <sporring@di.ku.dk>
2026-06-29 18:24:09 +0200

    minor updates

--- samples/createByEuler2DTransform/createByEuler2DTransform.fs
+++ samples/createByEuler2DTransform/createByEuler2DTransform.fs
@@ -13,14 +13,18 @@ let main arg =
         else
             64u, 64u, 64u, "../tmp/rotatingBoxes"
 
-    // Create square as a mask image from a polygon
+    // Create square as a mask image from a polygon in the middle of a slice
     let polygon : Polygon2D =
-        [ { X = 24.0; Y = 24.0 }
-          { X = 40.0; Y = 24.0 }
-          { X = 40.0; Y = 40.0 }
-          { X = 24.0; Y = 40.0 } ]
+        let n = min width height
+        let lower = n/2u - n/8u |> float
+        let upper = n/2u + n/8u |> float
+        [ { X = lower; Y = lower }
+          { X = upper; Y = lower }
+          { X = upper; Y = upper }
+          { X = lower; Y = upper } ]
     let mask = polygonMask width height polygon
 
+    // Make a function for transforming masks slices in interesting way
     let euler2DTransformPath (transformName: string) (i: uint) (chunk: StackCore.Chunk<uint8>) =
         let centerX = float width / 2.0 - 0.5
         let centerY = float height / 2.0 - 0.5
@@ -31,7 +35,7 @@ let main arg =
             match transformName with
             | "AntiDiagonal" -> (centerX, centerY, angle), (float width - dx - centerX, dx - centerY)
             | "TopDown" ->      (centerX, centerY, angle), (0.0, dx - centerY)
-            | _ ->              (centerX, centerY, angle), (0.0, 0.0)
+            | _ ->              (centerX, centerY, angle), (-centerX, -centerY)
         euler2DTransform<uint8> rotation translation chunk
 
     // Create 3 sources that are squares which move in different ways.
@@ -43,8 +47,10 @@ let main arg =
     let topDown =      movingMask "TopDown"
     let antiDiagonal = movingMask "AntiDiagonal"
 
+    // Max of a tripple fan-out
     zip (zip diagonal topDown >>=> maxOfPair<uint8>) antiDiagonal
     >>=> maxOfPair<uint8>
+    >=> intensityStretch 0.0 1.0 0.0 255.0
     >=> write output ".tiff"
     |> sink
 
